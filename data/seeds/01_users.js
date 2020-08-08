2583f2708587b745cda09eaa5a9167f230d39fae
Tiffany Arellano <missusfantastic@Missus-Fantastic.local>
2020-08-08 16:52:26 -0400

    MVP

--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -1,5 +1,46 @@
 exports.seed = async function (knex) {
-  await knex('users').insert([
-    { username: 'user', password: 'password', department: 'administration', role: 'admin' }
-  ])
-}
+  await knex("users").insert([
+    {
+      username: "dean",
+      password: "$2a$14$AYh7ALQodN5HPtWlTUQGyeSAWnoVvxcv4lOtnNWMUbMCYbdNI2hHi",
+      department: "administration",
+      role: "dean",
+    },
+    {
+      username: "tiff",
+      password: "$2a$14$NcqnuxWtVwpqlSQpu/B3peKe5ylbqVRjOxt6/f2AJkSItZz9L80Ke",
+      department: "art",
+      role: "teacher",
+    },
+    {
+      username: "tom",
+      password: "$2a$14$NcqnuxWtVwpqlSQpu/B3peKe5ylbqVRjOxt6/f2AJkSItZz9L80Ke",
+      department: "art",
+      role: "student",
+    },
+    {
+      username: "ben",
+      password: "$2a$14$VQoUHt3hbw8Au0Xi.arg.ekf5v7mCRSp/MFYXX1/GOZG/3gcdX5rG",
+      department: "english",
+      role: "student",
+    },
+    {
+      username: "bob",
+      password: "$2a$14$EPTe3.VvWLfUEQG8LarQUuqp/mG0BkZKHGF8FUwzVNjYKTDeNTfQK",
+      department: "english",
+      role: "teacher",
+    },
+    {
+      username: "stacy",
+      password: "$2a$14$9lYdQz4X4DTQenpSLD0ufuPMfjM7SowBqnZRLpBaeD7uDXu0ci90O",
+      department: "science",
+      role: "teacher",
+    },
+    {
+      username: "sam",
+      password: "$2a$14$J69NCUq/SWr.v/1SBoJSd..p2qrP5oPXbgxBzHT8336hY9Fc/X0hm",
+      department: "science",
+      role: "student",
+    },
+  ]);
+};
--- middlewares/restrict.js
+++ middlewares/restrict.js
@@ -1,31 +1,33 @@
-const jwt = require('jsonwebtoken')
-const { json } = require('express')
+const jwt = require("jsonwebtoken");
+const { json } = require("express");
 
-const roles = ['users', 'admin']
+const roles = ["student", "teacher", "dean"];
 
 function restrict(role) {
   return async (req, res, next) => {
-    const authError = { message: 'Invalid Credentials' }
+    const authError = { message: "Invalid Credentials" };
     try {
-      const token = req.cookies.token
+      const token = req.cookies.token;
       if (!token) {
-        return res.status(401).json(authError)
+        return res.status(401).json(authError);
       }
-      jwt.verify(token, 'keep it safe', (err, decoded) => {
+      jwt.verify(token, "keep it safe", (err, decoded) => {
         if (err) {
-          return res.status(401).json(authError)
+          return res.status(401).json(authError);
         }
 
         if (role && roles.indexOf(decoded.role) < roles.indexOf(role)) {
-          return res.status(401).json({ message: "You don't have access to this feature." })
+          return res
+            .status(401)
+            .json({ message: "You don't have access to this feature." });
         }
 
-        next()
-      })
+        next();
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 }
 
-module.exports = restrict
+module.exports = restrict;
