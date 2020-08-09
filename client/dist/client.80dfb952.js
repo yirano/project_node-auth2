1c666df67f5bef5a8e9c53e2216fd5879f18c3fd
yirano <yirano@protonmail.ch>
2020-08-08 20:44:13 -0400

    Update hash

--- client/dist/client.80dfb952.js
+++ client/dist/client.80dfb952.js
@@ -36988,7 +36988,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59309" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52191" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- routers/users-router.js
+++ routers/users-router.js
@@ -1,75 +1,75 @@
-const router = require("express").Router();
-const Users = require("../models/users-model.js");
-const restrict = require("../middlewares/restrict");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const router = require("express").Router()
+const Users = require("../models/users-model.js")
+const restrict = require("../middlewares/restrict")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 router.get("/", restrict("dean"), async (req, res, next) => {
   try {
-    res.json(await Users.getAll());
+    res.json(await Users.getAll())
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 router.get("/students", restrict("teacher"), async (req, res, next) => {
   try {
-    res.json(await Users.getStudents());
+    res.json(await Users.getStudents())
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 router.get("/department", restrict("student"), async (req, res, next) => {
   try {
-    const decode = jwt.decode(req.cookies.token);
-    res.json(await Users.getDepartment(decode.department));
+    const decode = jwt.decode(req.cookies.token)
+    res.json(await Users.getDepartment(decode.department))
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 router.post("/register", async (req, res, next) => {
   try {
-    const { username, password, department, role } = req.body;
+    const { username, password, department, role } = req.body
 
-    const user = await Users.findBy(username).first();
+    const user = await Users.findBy(username).first()
 
     if (user) {
-      return res.status(409).json({ message: "Username is already taken" });
+      return res.status(409).json({ message: "Username is already taken" })
     }
 
     const newUser = await Users.add({
       username,
-      password: await bcrypt.hash(password, 14),
+      password: await bcrypt.hash(password, 10),
       department,
       role,
-    });
+    })
 
-    res.status(201).json(newUser);
+    res.status(201).json(newUser)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 router.post("/login", async (req, res, next) => {
   try {
-    const { username, password } = req.body;
-    const user = await Users.findBy(username).first();
+    const { username, password } = req.body
+    const user = await Users.findBy(username).first()
 
     if (!user) {
       return res.status(401).json({
         message: "Invalid Credentials 1",
-      });
+      })
     }
 
     // hash the password again and see if it matches what we have in the database
-    const passwordValid = await bcrypt.compare(password, user.password);
+    const passwordValid = await bcrypt.compare(password, user.password)
 
     if (!passwordValid) {
       return res.status(401).json({
         message: "Invalid Credentials 2",
-      });
+      })
     }
 
     const payload = {
@@ -77,24 +77,24 @@ router.post("/login", async (req, res, next) => {
       username: user.username,
       department: user.department,
       role: user.role, // this value would usually come from the database
-    };
+    }
 
-    res.cookie("token", jwt.sign(payload, process.env.SECRET));
+    res.cookie("token", jwt.sign(payload, process.env.SECRET))
     res.json({
       message: `Welcome ${user.username}!`,
       id: user.id,
-    });
+    })
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 router.get("/logout", async (req, res, next) => {
   try {
-    res.clearCookie("token").end();
+    res.clearCookie("token").end()
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
