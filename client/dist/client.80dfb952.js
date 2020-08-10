b643ad3e5f3aff18391171720673b03bd0a2a1e7
yirano <yirano@protonmail.ch>
2020-08-10 12:29:51 -0400

    Update .cache

--- client/dist/client.80dfb952.js
+++ client/dist/client.80dfb952.js
@@ -41837,7 +41837,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61567" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51423" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
